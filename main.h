506f2c384406742fd1a7e003fc37e8b5895443ef
mwambajoakim <mwambajoakim@gmail.com>
2023-06-27 18:52:41 +0300

    The header file

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct format - This will match the specifiers to the function
+ * @spec: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @spec_func: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *spec;
+	int (*spec_func)();
+} conversion_spec;
+
+int put_char(char c);
+int _printf(const char *format, ...);
+int printfstring(va_list value);
+int printchar(va_list value);
+int print_percent(void);
+
+
+#endif

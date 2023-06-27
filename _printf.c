935be158f1cb146414cfc3a8eb7fe46f180761f8
mwambajoakim <mwambajoakim@gmail.com>
2023-06-27 19:00:05 +0300

    The _printf function

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
+ */
+int _printf(const char * const format, ...)
+{
+	conversion_spec m[] = {
+		{"%s", printfstring}, {"%c", printchar},
+		{"%%", print_percent},
+		};
+
+	va_list arguments;
+	int j = 0, k, len = 0;
+
+	va_start(arguments, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	Here:
+	while (format[j] != '\0')
+	{
+		k = 13;
+		while (k >= 0)
+		{
+			if (m[k].spec[0] == format[j] && m[k].spec[1] == format[j + 1])
+			{
+				len += m[k].spec_func(arguments);
+				j = j + 2;
+				goto Here;
+			}
+			k--;
+		}
+		_putchar(format[j]);
+		len++;
+		j++;
+	}
+	va_end(arguments);
+	return (len);
+}

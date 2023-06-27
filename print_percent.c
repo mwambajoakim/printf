b658713dfa9a921e3a3723c6c568fb85ee650567
mwambajoakim <mwambajoakim@gmail.com>
2023-06-27 18:36:17 +0300

    A function that prints a percent sign

---
+++ print_percent.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_percent - a function that prints the percent sign
+ * Return: 1 is success
+*/
+
+int print_percent(void)
+{
+	_putchar(37);
+	return (1);
+}

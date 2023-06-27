f360eb3ea2e641347c4838a22bdd7dd9e89a788d
mwambajoakim <mwambajoakim@gmail.com>
2023-06-27 18:29:42 +0300

    A function that prints a string

---
+++ printstring.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ *str_len - Function that returns the length of a string
+ *
+ *@s: string to be tested
+ *
+ *Return: 0 is success
+ *
+ */
+
+int str_len(char *s)
+{
+char *len = s;
+if (*len != '\0')
+{
+	len++;
+}
+return (len - s);
+}
+
+/**
+ * printfstring - a function that prints a string
+ * @value: argument passed to string
+ * Return: the length of the string
+ */
+
+int printfstring(va_list value)
+{
+	char *s;
+	int i, length;
+
+	s = va_arg(value, char *);
+	if (s == NULL)
+	{
+		s = "(null)";
+		return (0);
+	}
+	else
+	{
+		length = str_len(s);
+		for (i = 0; i < length; i++)
+			_putchar(s[i]);
+		return (length);
+	}
+}

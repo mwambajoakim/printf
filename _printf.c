6bb5b5e04f7f7f695701778eabbaff10da72d794
mwamba <mwambajoakim@gmail.com>
2025-04-14 16:52:08 +0300

    Describe function working and parameters

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _printf - Prints formmated data to stdout.
+ * @format: Pointer to different formats.
+ *
+ * Return: Number of characters printed to stdout.
+ */
+int _printf(const char *format, ...)
+{}

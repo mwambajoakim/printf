6df61f6440e65954e9f27118c6acf9b11ec149e4
mwamba <mwambajoakim@gmail.com>
2025-04-14 17:43:14 +0300

    Handle character case c

--- _printf.c
+++ _printf.c
@@ -7,4 +7,30 @@
  * Return: Number of characters printed to stdout.
  */
 int _printf(const char *format, ...)
-{}
+{
+	int len;
+	va_list args;
+	char *fmt_spec = "%";
+
+	if (format == NULL)
+	{
+		fprintf(stderr, "%s", "Null format string\n");
+		return (1);
+	}
+	va_start(args, format);
+	while (format)
+	{
+		if (format == fmt_spec)
+			format++;
+		switch(format)
+		{
+			case 'c':
+				printf("%c"va_arg(args, int));
+				break;
+		}
+		format++;
+	}
+	len = strlen(format);
+	va_end(args);
+	return (len);
+}
